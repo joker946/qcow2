11aa06c6b032098110c9680703d21d033ece9397
Joker946 <joker946@gmail.com>
2014-06-13 13:21:49 +0400

    stable beta 0.1

--- qcowParse.py
+++ qcowParse.py
@@ -17,38 +17,55 @@ def getInfo (file, begin, read, paramOfUnpack):
 	info_ = file.read(read)
 	info = struct.unpack(paramOfUnpack, info_)
 	return 	str(info[0])
-def getBFName (file, backing_file_offset_start):
+def getBFName (file, backing_file_offset_start, backing_file_size):
 	if (int(backing_file_offset_start)==0):
 		return 'There is not any backing file found'
 	else:
-		file.seek(int(backing_file_offset_start))
-		i=1
-		info_ = file.read(i)# read first char
-		while (struct.unpack('c', info_)[0]!='\x00'): #until we have met 00 as hex
-			i+=1
-			info_ = file.read(1)
-		file.seek(int(backing_file_offset_start))
-		info_ = file.read(i)
-		info = struct.unpack(str(i)+'s', info_)
+		intBFOffset = int (backing_file_offset_start)
+		intBFSize = int (backing_file_size) 
+
+		file.seek(intBFOffset)
+		info_ = file.read(intBFSize) #read all backing file bytes
+		info = struct.unpack(str(intBFSize)+'s', info_)
 		return str(info[0])
-def getSnapshotId(file, ss_offset, x):
-	offset = 64 * x - 8
-	file.seek(int(int(ss_offset)+offset))
-	value = file.read(1)
-	return value
+def getSnapshot(file, ss_offset):
+	file.seek(int(ss_offset)+12)
+	len_id_ = file.read(2)
+	len_id = struct.unpack('>H', len_id_)
+	file.seek(int(ss_offset)+14)
+	len_name_ = file.read(2)
+	len_name = struct.unpack('>H', len_name_)
+	file.seek(int(ss_offset)+36)
+	ex_data_size_ = file.read(4)
+	ex_data_size = struct.unpack('>I', ex_data_size_)
+	file.seek(int(int(ss_offset)+40+int(ex_data_size[0])))
+	ss_id_ = file.read(int(len_id[0]))
+	ss_id = struct.unpack('c', ss_id_)
+	file.seek(int(int(ss_offset)+40+int(ex_data_size[0])+len_id[0]))
+	ss_name_ = file.read(int(len_name[0]))
+	ss_name  =struct.unpack(str(len_name[0])+'s', ss_name_)
+	ssobj = {'ss_id': ss_id[0], 'ss_name':ss_name[0]}
+	currentlength = int(int(ss_offset)+40+int(ex_data_size[0])+len_id[0]+len_name[0])
+	while (currentlength%8!=0):
+		currentlength+=1
+	ssobj = {'ss_id': ss_id[0], 'ss_name':ss_name[0], 'curlen':currentlength}
+	return ssobj
 def Print():
 	nb_ss = getInfo(f, 60, 4, '>I') #number of snapshots
 	ss_offset = getInfo(f, 64, 8, '>Q')
 	print 'magic: ' + getInfo (f, 0, 4, '4s')  #magic
 	print 'Filename: ' + str(os.path.abspath(currentpath))
 	print 'Version: ' + getInfo (f, 4, 4, '>I')  #version
-	print 'Backing File path: ' + getBFName(f, getInfo(f, 8, 8, '>Q'))
+	print 'Backing File path: ' + getBFName(f, getInfo(f, 8, 8, '>Q'), getInfo(f, 16, 4, '>I'))
 	print 'Virtual Size (byte): ' + getInfo (f, 24, 8, '>Q')  #vsize
 	print 'Size (byte): ' + str(os.stat(currentpath).st_size) #size
 	print 'Number of snapshots: ' + nb_ss
 	print 'Snapshots offset: ' + getInfo(f, 64, 8, '>Q')
 	for x in range(1, int(nb_ss)+1):
-		print '-- Snapshot ID: ' + getSnapshotId(f, ss_offset, x)
+		ss = getSnapshot(f, ss_offset)
+		print '-- Snapshot ID: ' + str(ss['ss_id'])
+		print '-- Snapshot Name: ' + str(ss['ss_name'])
+		ss_offset = ss['curlen']
 def Main():
 	backing_file_offset_start = getInfo(f, 8,8,'>Q') # trying to get BF offset. It returns '0' if isn't found
 	ss_offset = getInfo(f, 64, 8, '>Q')
